00781af86b4734ae6b50e8e108869ec1450ae441
lonperman <lonperman@gmail.com>
2022-01-02 00:08:33 -0500

    Utilizando punteros para cambiar los valores en las referencias

--- tareas.go
+++ tareas.go
@@ -8,11 +8,26 @@ type task struct {
 	completado  bool
 }
 
+func (t *task) marcarCompletada(){
+	t.completado = true
+}
+
+func (t *task) actualizarDescripcion(descripcion string){
+	t.descripcion = descripcion
+}
+
+func (t *task) actualizarNombre(nombre string){
+	t.nombre = nombre
+}
+
 func main() {
-	t := task{
+	t := &task{
 		nombre:      "Completar mi curso de Go",
 		descripcion: "Completar mi curso esta semana",
 	}
-	fmt.Println(t)
+	fmt.Printf("%+v\n", t)
+	t.marcarCompletada()
+	t.actualizarNombre("Finalizar mi curso")
+	t.actualizarDescripcion("Completar mi curso de manera rapida")
 	fmt.Printf("%+v\n", t)
 }
